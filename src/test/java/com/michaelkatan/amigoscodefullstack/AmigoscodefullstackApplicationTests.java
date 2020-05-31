235a4fafbf0abe7044737fe0b952ed8d1ad79eee
Michael Katan <michaelkatan@gmail.com>
2020-06-01 00:57:10 +0300

    Initial commit

---
+++ src/test/java/com/michaelkatan/amigoscodefullstack/AmigoscodefullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelkatan.amigoscodefullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodefullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
