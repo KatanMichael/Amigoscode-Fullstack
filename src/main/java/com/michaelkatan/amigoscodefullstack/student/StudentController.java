6ecbad18b8604dea27e9e782f8aa1cc38fcfb49d
Michael Katan <michaelkatan@gmail.com>
2020-06-01 01:47:42 +0300

    React added to the project

---
+++ src/main/java/com/michaelkatan/amigoscodefullstack/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.michaelkatan.amigoscodefullstack.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController
+{
+    @GetMapping()
+    public List<Student> getAllStudents()
+    {
+        ArrayList list = new ArrayList<>();
+        list.add(new Student(UUID.randomUUID(), "Michael",
+                "Katan","Email", Student.Gender.MALE));
+        return list;
+    }
+}
